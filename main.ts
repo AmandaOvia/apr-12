7588c209cadbba896d11e42974298723ed7c49c5
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-12 09:41:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 999; index++) {
+        number += 1
+        sum = number + sum
+    }
+    basic.showNumber(sum)
+})
+// 1+2+3+4+5+6..+999
+let sum = 0
+let number = 0
+number = 0
+sum = 0
